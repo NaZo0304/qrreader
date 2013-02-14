31a03acb31dc54a38f2ba2ded18fb266c89cc26a
masato kawakami <mkawakami0304@gmail.com>
2013-02-14 12:42:58 +0900

    Run Success

--- src/com/example/com/childhood/rssreader/RSSActivity.java
+++ src/com/example/com/childhood/rssreader/RSSActivity.java
@@ -16,7 +16,6 @@ public class RSSActivity extends DroidGap {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_rss, menu);
 		return true;
 	}
 
